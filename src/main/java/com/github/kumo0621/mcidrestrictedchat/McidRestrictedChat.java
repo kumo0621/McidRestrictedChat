8b0e87fbf5e096f02346a666728a5870cae31d47
kumo0621 <kumokumokumo0621@gmail.com>
2023-02-14 00:31:23 +0900

    テキストメッセージ変更

--- src/main/java/com/github/kumo0621/mcidrestrictedchat/McidRestrictedChat.java
+++ src/main/java/com/github/kumo0621/mcidrestrictedchat/McidRestrictedChat.java
@@ -1,6 +1,7 @@
 package com.github.kumo0621.mcidrestrictedchat;
 
 import net.kunmc.lab.commandlib.CommandLib;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.player.PlayerChatEvent;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -27,8 +28,18 @@ public final class McidRestrictedChat extends JavaPlugin implements org.bukkit.e
             String args = e.getMessage();
             String name = e.getPlayer().getName();
             int length = args.length();
-            if (name.length() != length) {
+            int namelength = name.length();
+            if (namelength != length) {
                 e.setCancelled(true);
+                length -= namelength;
+                int set = Math.abs(length);
+                int henkan = 0;
+                Player player = e.getPlayer();
+                if(henkan >= length) {
+                    player.sendMessage(set + "文字足りません");
+                }else{
+                    player.sendMessage(length + "文字多いです");
+                }
             }
         }
     }
